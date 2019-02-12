c7b43f5c34a15eb9f768a31bf4c3fc366a450c7a
neittien0110 <neittien0110@gmail.com>
2019-02-13 00:32:19 +0700

    Scan all pptx in a directory (input from command line -d)

--- powerpoint2pdf/Program.cs
+++ powerpoint2pdf/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jokedst.GetOpt;  //Sử dụng NuGet để cài đặt thêm gói Jokedst.GetOpt
 
 namespace powerpoint2pdf
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string InputDirectory = string.Empty;        /// input directory contains pptx files
+
+            /// Use GetOpt function from the lib NuGet Jokedst.GetOpt to analyse commandline params
+            try
+            {
+                var opts = new GetOpt("Convert PPTX to PDF", new[]
+                {
+                    new CommandLineOption('d', "--diretory", "input directory contains pptx files",  ParameterType.String, d => InputDirectory = (string)d)
+                });
+
+                opts.ParseOptions(args);
+
+                if (InputDirectory==string.Empty)
+                {
+                    InputDirectory = Directory.GetCurrentDirectory();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return e.HResult;
+            }
+
+            //Open and scan all pptx files
+            DirectoryInfo dir = new DirectoryInfo(InputDirectory);
+
+            foreach (FileInfo f in dir.GetFiles("*.pptx"))
+            {
+                Console.WriteLine("File {0}", f.FullName);
+            }
+
+            return 0;
         }
     }
 }
