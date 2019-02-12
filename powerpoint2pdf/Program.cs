2081ea171737bd399e648d46ee78df7df1e47f65
neittien0110 <neittien0110@gmail.com>
2019-02-12 22:55:44 +0700

    Create empty project

---
+++ powerpoint2pdf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powerpoint2pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
